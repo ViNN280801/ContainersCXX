7800bf718364c902096613d5b96920f6fc3b3a14
Vladislav <vladislav_semykin01@mail.ru>
2023-04-16 13:25:21 +0300

    Added bash script which will autocompile and autolink project to an executive file

---
+++ Binary Tree/autoconf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Do you want to use GNU GCC (1) or CMake (2). Exit (#): "
+while read choice; do
+    if [[ "$choice" == "1" ]]; then
+        gcc -g -c bintree.cpp -lstdc++ -std=c++23 -Wall -Wpedantic -Wextra -o bintree.o
+        gcc -g -c main.cpp -lstdc++ -std=c++23 -Wall -Wpedantic -Wextra -o main.o
+        gcc main.o bintree.o -lstdc++ -std=c++23 -o main
+        rm bintree.o main.o
+        ./main
+        break
+    elif [[ "$choice" == "2" ]]; then
+        cmake .
+        cmake --build .
+        ./main
+        break
+    elif [[ "$choice" == "#" ]]; then
+        echo "Exiting..."
+        break
+    fi
+done
