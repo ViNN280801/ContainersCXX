f15333fc23b5766a14655d369c63b479139b18cd
Vladislav <vladislav_semykin01@mail.ru>
2023-04-18 22:25:37 +0300

    Script corrected (because bintree.cpp deleted)

--- Binary Tree/autoconf.sh
+++ Binary Tree/autoconf.sh
@@ -3,10 +3,9 @@
 echo "Do you want to use GNU GCC (1) or CMake (2). Exit (#): "
 while read choice; do
     if [[ "$choice" == "1" ]]; then
-        gcc -g -c bintree.cpp -lstdc++ -std=c++17 -Wall -Wpedantic -Wextra -o bintree.o
         gcc -g -c main.cpp -lstdc++ -std=c++17 -Wall -Wpedantic -Wextra -o main.o
-        gcc main.o bintree.o -lstdc++ -std=c++17 -o main
-        rm bintree.o main.o
+        gcc main.o -lstdc++ -std=c++17 -o main
+        rm main.o
         ./main
         break
     elif [[ "$choice" == "2" ]]; then
