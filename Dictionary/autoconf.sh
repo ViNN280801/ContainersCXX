dab047585c573484bc3ac368c0219521e0c72926
Vladislav <vladislav_semykin01@mail.ru>
2023-04-18 22:23:43 +0300

    CMake file and autocompiling script corrected

--- Dictionary/autoconf.sh
+++ Dictionary/autoconf.sh
@@ -3,10 +3,9 @@
 echo "Do you want to use GNU GCC (1) or CMake (2). Exit (#): "
 while read choice; do
     if [[ "$choice" == "1" ]]; then
-        gcc -g -c src/dictionary.cpp -lstdc++ -std=c++23 -Wall -Wpedantic -Wextra -o dict.o
         gcc -g -c main.cpp -lstdc++ -std=c++23 -Wall -Wpedantic -Wextra -o main.o
-        gcc dict.o main.o -lstdc++ -std=c++23 -o main
-        rm dict.o main.o
+        gcc main.o -lstdc++ -std=c++23 -o main
+        rm main.o
         ./main
         break
     elif [[ "$choice" == "2" ]]; then
