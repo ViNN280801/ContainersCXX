e76261334aa85168e17ef7165fd7ecd5c84202ea
Vladislav <vladislav_semykin01@mail.ru>
2023-04-18 22:20:31 +0300

    Added tests for dictionary

--- Dictionary/main.cpp
+++ Dictionary/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 
-#include "dictionary.hpp"
-#include "dictionary_impl.hpp"
+#include "include/dictionary.hpp"
+#include "include/dictionary_impl.hpp"
+
+using namespace std::string_literals;
 
 int main()
 {
-    dictionary<char, int> dict;
+    Dictionary<int, std::string> dict(4, "This is a string"s);
+    std::cout << dict.at(4) << std::endl;
+    std::cout << dict.get(4) << std::endl;
+    // std::cout << dict.at(5) << std::endl;
+    std::cout << dict.get(100) << std::endl;
 
-    std::cout << "Hello, World!" << std::endl;
+    dict.set(4, "new string");
+    std::cout << dict.at(4) << std::endl;
+
+    std::cout << dict.is_set(4) << '\t' << dict.is_set(45) << std::endl;
+    dict.get_key();
+
     return 0;
 }
