e998743491ee9ed53f9baf7ee1871c9fea0eda50
Vladislav <vladislav_semykin01@mail.ru>
2023-04-21 14:44:03 +0300

    Autocompiling file added

---
+++ Stack/autoconf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Do you want to use GNU GCC (1) or CMake (2). Exit (#): "
+while read choice; do
+    if [[ "$choice" == "1" ]]; then
+        gcc -g -c main.cpp -lstdc++ -std=c++14 -Wall -Wpedantic -Wextra -o main.o
+        gcc main.o -lstdc++ -std=c++14 -o main
+        rm main.o
+        ./main
+        break
+    elif [[ "$choice" == "2" ]]; then
+        cmake .
+        cmake --build .
+        ./main
+        break
+    elif [[ "$choice" == "#" ]]; then
+        echo "Exiting..."
+        break
+    fi
+done
